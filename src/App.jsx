5934ce0c086c3eb465e1604586eb82c7c7c032dc
Matt Parrett <matt.parrett@gmail.com>
2025-01-20 16:50:46 -0800

    Revert "Build a revert commit"
    This reverts commit 9d47be87c504c5453c71af3d1d6a15a34cac0be7.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
 
-import { HashRouter } from 'react-router-dom'; // Note 1
-
 import SimonGame from './components/SimonGame'
 
 function App() {
-	return (
-		<HashRouter basename="/simon-game">
-			<div className="min-h-screen bg-gray-100 dark:bg-gray-800 p-4">
-				<SimonGame />
-			</div>
-		</HashRouter>
-	)
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 p-4">
+      <SimonGame />
+    </div>
+  )
 }
 
 
