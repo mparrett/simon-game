862df5948cdf1ff7c5375a3bb405d8519f23e328
Matt Parrett <matt.parrett@gmail.com>
2025-01-20 16:10:29 -0800

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import SimonGame from './components/SimonGame'
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-800 p-4">
-      <SimonGame />
-    </div>
-  )
+	return (
+		<Router basename="/simon-game">
+			<div className="min-h-screen bg-gray-100 dark:bg-gray-800 p-4">
+				<SimonGame />
+			</div>
+		</Router>
+	)
 }
 
 
